4ff30b43f61057ea4a768579e10e6bfc36703bb7
eds-tcz <edstechchefz@gmail.com>
2024-11-06 02:05:06 +0530

    bug fix

--- blocks/mlow-market-sentiments/mlow-market-sentiments.js
+++ blocks/mlow-market-sentiments/mlow-market-sentiments.js
@@ -1,39 +1,39 @@
 export default function decorate(block) {
   [...block.children].forEach((row, r) => {
-    row.classList.add("mlow-ms-section");
+    row.classList.add('mlow-ms-section');
     if (r === 0) {
-      row.classList.add("advance-decline");
-      const table = row.querySelector("table");
+      row.classList.add('advance-decline');
+      const table = row.querySelector('table');
       if (table) {
-        table.classList.add("ad-tab");
+        table.classList.add('ad-tab');
         [...table.rows].forEach((tr) => {
-          tr.classList.add("ad-row");
+          tr.classList.add('ad-row');
           [...tr.cells].forEach((td) => {
-            td.classList.add("ad-cell");
+            td.classList.add('ad-cell');
           });
         });
       }
     }
     if (r === 1) {
-      row.classList.add("market-sentiments");
+      row.classList.add('market-sentiments');
       [...row.children].forEach((div, d) => {
         if (d === 0) {
-          div.classList.add("ms-heading");
+          div.classList.add('ms-heading');
         }
         if (d === 1) {
-          div.classList.add("ms-head-filter");
+          div.classList.add('ms-head-filter');
         }
         if (d === 2) {
-          div.classList.add("ms-content");
+          div.classList.add('ms-content');
           // Remove the existing <p> tag inside ms-content
-          const pTag = div.querySelector("p");
+          const pTag = div.querySelector('p');
           if (pTag) {
             pTag.remove();
           }
 
           // Create and insert the circular chart
-          const circularChart = document.createElement("div");
-          circularChart.classList.add("circular-chart");
+          const circularChart = document.createElement('div');
+          circularChart.classList.add('circular-chart');
           circularChart.innerHTML = `
             <div class="chart-content">
               <span>Bearish</span>
@@ -43,14 +43,14 @@ export default function decorate(block) {
           div.appendChild(circularChart);
         }
         if (d === 3) {
-          div.classList.add("ms-status");
-          const tableA = div.querySelector("table");
+          div.classList.add('ms-status');
+          const tableA = div.querySelector('table');
           if (tableA) {
-            tableA.classList.add("ms-status-tab");
+            tableA.classList.add('ms-status-tab');
             [...tableA.rows].forEach((tr, t) => {
-              tr.classList.add("ms-status-row-".concat(t + 1));
+              tr.classList.add('ms-status-row-'.concat(t + 1));
               [...tr.cells].forEach((td, i) => {
-                td.classList.add("ms-status-cell");
+                td.classList.add('ms-status-cell');
               });
             });
           }
@@ -58,42 +58,42 @@ export default function decorate(block) {
       });
     }
     if (r === 2) {
-      row.classList.add("cash-flows");
+      row.classList.add('cash-flows');
       [...row.children].forEach((div, d) => {
         if (d === 0) {
-          div.classList.add("cf-heading");
-          const table = div.querySelector("table");
+          div.classList.add('cf-heading');
+          const table = div.querySelector('table');
           if (table) {
-            table.classList.add("cf-status-tab");
+            table.classList.add('cf-status-tab');
             [...table.rows].forEach((tr, t) => {
-              tr.classList.add("cf-status-row-".concat(t + 1));
+              tr.classList.add('cf-status-row-'.concat(t + 1));
               [...tr.cells].forEach((td, i) => {
-                td.classList.add("cf-status-cell");
+                td.classList.add('cf-status-cell');
               });
             });
           }
         }
         if (d === 1) {
-          div.classList.add("cf-graph");
+          div.classList.add('cf-graph');
         }
         if (d === 2) {
-          div.classList.add("cf-heading-2");
-          const table = div.querySelector("table");
+          div.classList.add('cf-heading-2');
+          const table = div.querySelector('table');
           if (table) {
-            table.classList.add("cf-status-tab");
+            table.classList.add('cf-status-tab');
             [...table.rows].forEach((tr, t) => {
-              tr.classList.add("cf-status-row-".concat(t + 1));
+              tr.classList.add('cf-status-row-'.concat(t + 1));
               [...tr.cells].forEach((td, i) => {
-                td.classList.add("cf-status-cell");
+                td.classList.add('cf-status-cell');
               });
             });
           }
         }
         if (d === 3) {
-          div.classList.add("cf-graph");
+          div.classList.add('cf-graph');
         }
         if (d === 4) {
-          div.classList.add("cf-button");
+          div.classList.add('cf-button');
         }
       });
     }
