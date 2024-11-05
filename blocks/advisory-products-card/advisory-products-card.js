2b6d05ef09c204fc6dfc06b915c0501b60227d36
pankajTCZ <pankaj.datt@techchefz.com>
2024-11-05 21:19:57 +0530

    CSS Code fix

--- blocks/advisory-products-card/advisory-products-card.js
+++ blocks/advisory-products-card/advisory-products-card.js
@@ -9,15 +9,19 @@ export default function decorate(block) {
         div.classList.add('mlow-heading-h2'); // Add mlow-heading-h2 to the first div
       }
       if (d === 1) {
-        div.classList.add('mlow-button-view-more'); // Add mlow-button-view-more to the second div
+        div.classList.add('mlow-button-view-more');
         
-        // Optional: Check for 'a' tag inside and remove 'button' class
         const aTag = div.querySelector('a');
         if (aTag && aTag.classList.contains('button')) {
           aTag.classList.remove('button');
         }
       }
-      // You can extend this pattern for any other divs (e.g., for the third or fourth divs) if needed
+      if (d === 2) { 
+        div.classList.add('advisory-div3'); // Add mlow-heading-h2 to the first div
+      }
+      if (d === 3) { 
+        div.classList.add('adisory-div4'); // Add mlow-heading-h2 to the first div
+      }
     });
   });
 }
